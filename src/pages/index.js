43a00cb916d4cfccb274cc594ab318991c000df7
deannapoh <156443891+deannapoh@users.noreply.github.com>
2024-06-16 18:05:23 +0000

    Second commit - vercel problem fix

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import About from './About';
+import HelpUs from './HelpUs';
+import Home from './Home';
+import PetsPage from './PetsPage';
+import Volunteer from './Volunteer';
+
+export {
+    About,
+    HelpUs,
+    Home,
+    PetsPage,
+    Volunteer
+}
